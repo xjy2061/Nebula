9b2cb7909f7f1a9a5315a13211727f713d98638b
xjy2061 <xjy2061@gmail.com>
2018-06-05 14:45:58 +0800

    Add sp to px convert methods in DimensionUtils.

--- library/src/main/java/org/xjy/android/common/DimensionUtils.java
+++ library/src/main/java/org/xjy/android/common/DimensionUtils.java
@@ -21,4 +21,20 @@ public class DimensionUtils {
     public static int dpToIntPx(float dp, Context context) {
         return (int) (dpToFloatPx(dp, context) + 0.5);
     }
+
+    public static float spToFloatPx(float sp, DisplayMetrics displayMetrics) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, displayMetrics);
+    }
+
+    public static float spToFloatPx(float sp, Context context) {
+        return dpToFloatPx(sp, context.getResources().getDisplayMetrics());
+    }
+
+    public static int spToIntPx(float sp, DisplayMetrics displayMetrics) {
+        return (int) (dpToFloatPx(sp, displayMetrics) + 0.5);
+    }
+
+    public static int spToIntPx(float sp, Context context) {
+        return (int) (dpToFloatPx(sp, context) + 0.5);
+    }
 }
