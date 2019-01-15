12019f41bb3a479a7517a67f696e38f998499547
xjy2061 <xjy2061@gmail.com>
2019-01-15 19:36:02 +0800

    Add VerticalSwipeRefreshLayout.

---
+++ library/src/main/java/org/xjy/android/nebula/widget/VerticalSwipeRefreshLayout.java
@@ -0,0 +1,38 @@
+package org.xjy.android.nebula.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+public class VerticalSwipeRefreshLayout extends SwipeRefreshLayout {
+    private int mTouchSlop;
+    private float mDownX;
+
+    public VerticalSwipeRefreshLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public VerticalSwipeRefreshLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                mDownX = ev.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (Math.abs(ev.getX() - mDownX) > mTouchSlop) {
+                    return false;
+                }
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+}
